ba4807d3c2e8e73c0c7dc8e2e280310f49b3d977
Sirasit Punnanithi <spunnanithi@gmail.com>
2024-01-26 23:20:40 -0800

    Completed JS exercises 3-4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,8 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (arr, ...args) {
+	// For each of the values in the input arr,
+	// filter out the values that appear in the input
+	// optional arguments
+	return arr.filter((ele) => !args.includes(ele));
 };
 
 // Do not edit below this line
